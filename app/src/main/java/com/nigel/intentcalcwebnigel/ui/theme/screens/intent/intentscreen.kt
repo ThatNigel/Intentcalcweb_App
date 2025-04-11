b0ccf7ae1a3a359f650ab203ac3c9b06f8211ca4
ThatNigel <omondinigel04@gmail.com>
2025-04-11 11:08:02 +0300

    second commit

--- app/src/main/java/com/nigel/intentcalcwebnigel/ui/theme/screens/intent/intentscreen.kt
+++ app/src/main/java/com/nigel/intentcalcwebnigel/ui/theme/screens/intent/intentscreen.kt
@@ -1,9 +1,16 @@
 package com.nigel.intentcalcwebnigel.ui.theme.screens.intent
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.MediaStore
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
@@ -13,16 +20,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Intent_Screen(navController: NavHostController) {
-    Column (verticalArrangement = Arrangement.SpaceEvenly,
+    val context= LocalContext.current
+
+    Column (verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
@@ -32,10 +45,27 @@ fun Intent_Screen(navController: NavHostController) {
             fontSize = 30.sp,
             fontFamily = FontFamily.Cursive
         )
+        Spacer(modifier = Modifier.height(30.dp))
 
 
         OutlinedButton(
-            onClick = {/*TODO*/ },
+            onClick = {
+                val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" +"0769759883 "))
+                
+                if (ContextCompat.checkSelfPermission(
+                        context,
+                        android.Manifest.permission.CALL_PHONE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        context as Activity,
+                        arrayOf(android.Manifest.permission.CALL_PHONE),
+                        1
+                    )
+                } else {
+                    context.startActivity(intent)
+                }
+            },
             modifier = Modifier.width(300.dp),
             colors = ButtonDefaults.buttonColors(Color.DarkGray),
             shape = RoundedCornerShape(16.dp)
@@ -46,7 +76,17 @@ fun Intent_Screen(navController: NavHostController) {
                 fontSize = 30.sp
             )
         }
-        OutlinedButton(onClick = {/*TODO*/},
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedButton(onClick = {
+            val uri = Uri.parse("smsto:0769759883")
+
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+
+            intent.putExtra("Hello", "How is todays weather")
+
+            context.startActivity(intent)
+        },
             modifier=Modifier.width(300.dp),
             colors = ButtonDefaults.buttonColors(Color.DarkGray),
             shape = RoundedCornerShape(16.dp)
@@ -55,8 +95,20 @@ fun Intent_Screen(navController: NavHostController) {
             Text("Sms",
                 fontSize = 30.sp)
         }
+        Spacer(modifier = Modifier.height(10.dp))
+
         OutlinedButton(
-            onClick = {/*TODO*/ },
+            onClick = {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+
+                shareIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+                shareIntent.type = "text/plain"
+
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Hey, download this app!")
+
+                context.startActivity(shareIntent)
+            },
             modifier = Modifier.width(300.dp),
             colors = ButtonDefaults.buttonColors(Color.DarkGray),
             shape = RoundedCornerShape(16.dp)
@@ -67,8 +119,16 @@ fun Intent_Screen(navController: NavHostController) {
                 fontSize = 30.sp
             )
         }
+        Spacer(modifier = Modifier.height(10.dp))
+
         OutlinedButton(
-            onClick = {/*TODO*/ },
+            onClick = {
+                val phone = "0769759883"
+
+                val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+
+                context.startActivity(intent)
+            },
             modifier = Modifier.width(300.dp),
             colors = ButtonDefaults.buttonColors(Color.DarkGray),
             shape = RoundedCornerShape(16.dp)
@@ -79,9 +139,15 @@ fun Intent_Screen(navController: NavHostController) {
                 fontSize = 30.sp
             )
         }
+        Spacer(modifier = Modifier.height(10.dp))
+
 
         OutlinedButton(
-            onClick = {/*TODO*/ },
+            onClick = {
+                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                startActivityForResult(context as Activity,takePictureIntent,1,null)
+            },
             modifier = Modifier.width(300.dp),
             colors = ButtonDefaults.buttonColors(Color.DarkGray),
             shape = RoundedCornerShape(16.dp)
@@ -92,8 +158,17 @@ fun Intent_Screen(navController: NavHostController) {
                 fontSize = 30.sp
             )
         }
+        Spacer(modifier = Modifier.height(10.dp))
+
         OutlinedButton(
-            onClick = {/*TODO*/ },
+            onClick = {
+                val emailIntent=
+                    Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","omondinigel@gmail.com",null))
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT,"Subject")
+                    emailIntent.putExtra(Intent.EXTRA_TEXT,"Body")
+                    context.startActivity(emailIntent)
+
+            },
             modifier = Modifier.width(300.dp),
             colors = ButtonDefaults.buttonColors(Color.DarkGray),
             shape = RoundedCornerShape(16.dp)
@@ -104,9 +179,16 @@ fun Intent_Screen(navController: NavHostController) {
                 fontSize = 30.sp
             )
         }
+        Spacer(modifier = Modifier.height(10.dp))
+
 
         OutlinedButton(
-            onClick = {/*TODO*/ },
+            onClick = {
+                val simToolKitLaunchIntent =
+                    context.packageManager.getLaunchIntentForPackage("com.android.stk")
+
+                simToolKitLaunchIntent?.let { context.startActivity(it) }
+            },
             modifier = Modifier.width(300.dp),
             colors = ButtonDefaults.buttonColors(Color.DarkGray),
             shape = RoundedCornerShape(16.dp)
--- app/src/main/java/com/nigel/intentcalcwebnigel/ui/theme/screens/web/webscreen.kt
+++ app/src/main/java/com/nigel/intentcalcwebnigel/ui/theme/screens/web/webscreen.kt
@@ -1,5 +1,7 @@
 package com.nigel.intentcalcwebnigel.ui.theme.screens.web
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +13,10 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,11 +24,13 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Web_Screen(navController: NavHostController) {
+    var url by remember { mutableStateOf("")  }
     Column (verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()){
@@ -33,7 +41,9 @@ fun Web_Screen(navController: NavHostController) {
         Spacer(modifier=Modifier.height(40.dp))
 
         OutlinedButton(
-            onClick = {/*TODO*/},
+            onClick = {
+              url="https://www.google.com/"
+            },
             modifier = Modifier.width(300.dp),
             colors = ButtonDefaults.buttonColors(Color.DarkGray),
             shape = RoundedCornerShape(16.dp)
@@ -43,6 +53,16 @@ fun Web_Screen(navController: NavHostController) {
                 "Google ",
                 fontSize = 30.sp
             )
+        }
+        Spacer(modifier=Modifier.height(16.dp))
+        if(url.isEmpty()){
+            AndroidView(factory = {context ->
+                WebView(context).apply {
+                    webViewClient= WebViewClient()
+                    settings.javaScriptEnabled=true
+                loadUrl(url)
+                }
+            })
         }
 
 
